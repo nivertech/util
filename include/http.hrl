af1235fc31a25c4843f6938112c859eed98fbf70
Ori Bar <ori.bar@nivertech.com>
2012-07-29 17:00:08 +0300

    add HTTP_FORBIDDEN header

--- include/http.hrl
+++ include/http.hrl
@@ -32,6 +32,7 @@
 -define(HTTP_TEMPORARY_REDIRECT,    307).
 -define(HTTP_BAD_REQUEST,           400).
 -define(HTTP_UNAUTHORIZED,          401).
+-define(HTTP_FORBIDDEN,             403).
 -define(HTTP_NOT_FOUND,             404).
 -define(HTTP_METHOD_NOT_ALLOWED,    405).
 -define(HTTP_REQUEST_TIMEOUT,       408).
